72dca4d9958e8f83bec7cf15cd6a56847fd13709
MatiasDevop <solomatias321@gmail.com>
2022-12-21 22:02:16 +0100

    clean up

--- Candidates.Backend.TestApi/MockData/CandidateMockData.cs
+++ Candidates.Backend.TestApi/MockData/CandidateMockData.cs
@@ -1,10 +1,5 @@
 ﻿using Candidates.Backend.Application.Dtos;
 using Candidates.Backend.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Candidates.Backend.TestApi.MockData
 {
