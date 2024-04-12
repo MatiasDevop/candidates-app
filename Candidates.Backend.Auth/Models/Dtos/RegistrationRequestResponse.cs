67d03879e3769b04d559ea8b26503536031a8c56
Matt <solomatias321@gmail.com>
2024-04-12 10:15:32 +0200

    Auth with identity server

---
+++ Candidates.Backend.Auth/Models/Dtos/RegistrationRequestResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Candidates.Backend.Auth.Models.Dtos
+{
+    public class RegistrationRequestResponse : AuthResult
+    {
+    }
+}
--- Candidates.Backend.Auth/Program.cs
+++ Candidates.Backend.Auth/Program.cs
@@ -1,11 +1,16 @@
 using Candidates.Backend.Auth.Configurations;
 using Candidates.Backend.Auth.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
 
 builder.Services.AddDbContext<ApiDataContext>(options =>
@@ -13,19 +18,45 @@ builder.Services.AddDbContext<ApiDataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+        options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApiDataContext>();
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddAuthentication(options => // Auth mechanisn for my API
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+{
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = false,
+        RequireAudience = false,
+        RequireExpirationTime = false,
+        ValidateLifetime = false,
+    };
+});
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
